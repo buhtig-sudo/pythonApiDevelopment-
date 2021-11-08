ae3e623887e2a1b5105a69f534eda30dc222bacc
Vitaliy Azarov <azarovvit@mail.ru>
2021-11-08 16:45:48 +0300

    1

---
+++ Lesson04/Activity07/smilecook/app.py
@@ -0,0 +1,54 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_restful import Api
+
+from config import Config
+from extensions import db, jwt
+
+
+from resources.user import UserListResource, UserResource, MeResource
+from resources.token import TokenResource, RefreshResource, RevokeResource, black_list
+from resources.recipe import RecipeListResource, RecipeResource, RecipePublishResource
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    register_extensions(app)
+    register_resources(app)
+
+    return app
+
+
+def register_extensions(app):
+    db.init_app(app)
+    migrate = Migrate(app, db)
+    jwt.init_app(app)
+
+    @jwt.token_in_blocklist_loader #Все вхождения blacklist были переименованы в blocklist
+    def check_if_token_in_blocklist(jwt_headers=None, jwt_payload=None):
+        jti = jwt_payload['jti']
+        return jti in black_list
+
+
+def register_resources(app):
+    api = Api(app)
+
+    api.add_resource(UserListResource, '/users')
+    api.add_resource(UserResource, '/users/<string:username>')
+
+    api.add_resource(MeResource, '/me')
+
+    api.add_resource(TokenResource, '/token')
+    api.add_resource(RefreshResource, '/refresh')
+    api.add_resource(RevokeResource, '/revoke')
+
+    api.add_resource(RecipeListResource, '/recipes')
+    api.add_resource(RecipeResource, '/recipes/<int:recipe_id>')
+    api.add_resource(RecipePublishResource, '/recipes/<int:recipe_id>/publish')
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
